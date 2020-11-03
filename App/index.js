52f9171b0e504ea76d9e0fb79f9d420d951ad7e6
Ketan <ketantoo@gmail.com>
2020-11-03 13:02:39 +0530

    initial commit

---
+++ App/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { View, Text } from "react-native";
+import key from "../key";
+
+export default () => (
+  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <Text>View the finished branch for finished code.</Text>
+  </View>
+);
